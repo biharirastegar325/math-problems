f734507534ba311ae0f42ef790f05aa358debbd5
biharirastegar325 <biharirastegar325@gmail.com>
2025-03-20 18:41:44 +0000

    Add 15.java

---
+++ src/15.java
@@ -0,0 +1,9 @@
+public class MathProblem {
+    public static void main(String[] args) {
+        // Example Java code to solve a math problem
+        
+        double result = 5.0 * 3.0;
+        
+        System.out.println("The result is: " + result);
+    }
+}

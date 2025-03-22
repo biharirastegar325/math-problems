ea85c8b17bea8467d8130d1ae2d6bb52f2020995
biharirastegar325 <biharirastegar325@gmail.com>
2025-03-22 16:55:50 +0000

    Add 16.java

---
+++ src/16.java
@@ -0,0 +1,15 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example 1: Addition
+        System.out.println("The sum of 2 and 3 is: " + (2 + 3));
+
+        // Example 2: Subtraction
+        System.out.println("The difference between 7 and 3 is: " + (7 - 3));
+
+        // Example 3: Multiplication
+        System.out.println("The product of 5 and 4 is: " + (5 * 4));
+
+        // Example 4: Division
+        System.out.println("The result of 9 divided by 3 is: " + (9 / 3));
+    }
+}

7da0d4dc657d6716e876d2ddc50ce9d314237682
biharirastegar325 <biharirastegar325@gmail.com>
2025-03-24 09:53:31 +0000

    Add 18.java

---
+++ src/18.java
@@ -0,0 +1,7 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example of basic arithmetic operations using Java
+        int result = 5 + 3;
+        System.out.println("The sum is: " + result);
+    }
+}

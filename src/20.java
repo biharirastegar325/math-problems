05be7169be065a777ff2cc06349c4e0916aa73d8
biharirastegar325 <biharirastegar325@gmail.com>
2025-03-30 21:07:13 +0000

    Add 20.java

---
+++ src/20.java
@@ -0,0 +1,20 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example code to demonstrate various mathematical operations
+
+        System.out.println("Adding 10 and 20: " + (10 + 20));
+        System.out.println("Subtracting 8 from 34: " + (34 - 8));
+        System.out.println("Multiplying 5 by 6: " + (5 * 6));
+        System.out.println("Dividing 20 by 4: " + (20 / 4));
+
+        // Example code demonstrating loops
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}

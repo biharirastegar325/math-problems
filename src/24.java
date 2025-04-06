fb78cd22bc2e8ccaf9784b2051535981ce9c5ca6
biharirastegar325 <biharirastegar325@gmail.com>
2025-04-06 06:52:37 +0000

    Add 24.java

---
+++ src/24.java
@@ -0,0 +1,9 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example mathematical operations and calculations
+        System.out.println("Sum of 1+2: " + (1 + 2));
+        System.out.println("Difference between 5-3: " + (5 - 3));
+        System.out.println("Product of 7*4: " + (7 * 4));
+        System.out.println("Quotient and remainder of 10/3: " + (10 / 3));
+    }
+}

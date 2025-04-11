8eb24aac390619cd6d5b65ace3814d2335f21e96
biharirastegar325 <biharirastegar325@gmail.com>
2025-04-11 11:53:43 +0000

    Add 28.java

---
+++ src/28.java
@@ -0,0 +1,13 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example of a simple mathematical expression
+        System.out.println("The result of 5 + 3 is: " + (5 + 3));
+        
+        // Example of an arithmetic operation with floating-point numbers
+        double num1 = 4.9;
+        double num2 = 3.8;
+        double result = num1 / num2; // The result of 4.9 / 3.8 is approximately 1.378690563.
+        
+        System.out.println("The result of " + num1 + " divided by " + num2 + " is: " + result);
+    }
+}

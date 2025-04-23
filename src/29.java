d4a4ce76967ba9ec91d8e9d80dafa7adfd474f79
biharirastegar325 <biharirastegar325@gmail.com>
2025-04-23 10:49:01 +0000

    Add 29.java

---
+++ src/29.java
@@ -0,0 +1,9 @@
+public class MathProblemSolver {
+    public static void main(String[] args) {
+        // Example problem: Calculate the area of a rectangle
+        double length = 10.0;
+        double width = 5.0;
+        double area = length * width;
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}

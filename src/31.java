d55176d1c109b122245f13b8246093357131c215
biharirastegar325 <biharirastegar325@gmail.com>
2025-04-26 00:38:31 +0000

    Add 31.java

---
+++ src/31.java
@@ -0,0 +1,10 @@
+public class MathProblemSolver {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+
+        double result = a + b; // Sum of two numbers
+
+        System.out.println("The sum of " + a + " and " + b + " is: " + result);
+    }
+}

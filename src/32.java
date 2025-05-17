b63b760164501a7cf4fd7889dde822859860ee2e
biharirastegar325 <biharirastegar325@gmail.com>
2025-05-17 04:12:33 +0000

    Add 32.java

---
+++ src/32.java
@@ -0,0 +1,18 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example of simple arithmetic operations
+        int result1 = 2 + 3;  // 5
+        System.out.println("The result is: " + result1);
+        
+        double result2 = 4 - 2.5;  // 1.5
+        System.out.println("The result is: " + result2);
+        
+        float result3 = 9 / 3f;  // 3.0
+        
+        if (result3 > 5) {
+            System.out.println("Result of the if statement is greater than 5.");
+        } else {
+            System.out.println("Result of the if statement is not greater than 5.");
+        }
+    }
+}

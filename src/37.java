23527e5608c456ac00d0d650bc5f70029d6d8aa1
biharirastegar325 <biharirastegar325@gmail.com>
2025-05-20 07:05:06 +0000

    Add 37.java

---
+++ src/37.java
@@ -0,0 +1,6 @@
+public class MathProblemSolver {
+    public static void main(String[] args) {
+        double x = 3.5;
+        System.out.println("The square root of " + x + " is " + Math.sqrt(x));
+    }
+}

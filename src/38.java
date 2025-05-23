96fd979311dcd86d4cab5a6dc778cda830a34a2f
biharirastegar325 <biharirastegar325@gmail.com>
2025-05-23 07:17:25 +0000

    Add 38.java

---
+++ src/38.java
@@ -0,0 +1,19 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example Java program to solve some math-related problems
+
+        System.out.println("Hello World!");
+        
+        double a = 3.5;
+        double b = 2.0;
+        double c = 4.0;
+
+        double sum = a + b + c;
+        double difference = a - b + c;
+        double product = a * b + c;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+    }
+}

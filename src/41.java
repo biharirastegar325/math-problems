d75c4f61a1203558dd309c0014501c590d8b3164
biharirastegar325 <biharirastegar325@gmail.com>
2025-05-27 22:46:51 +0000

    Add 41.java

---
+++ src/41.java
@@ -0,0 +1,21 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Random number generation
+        int randomNumber = (int) (Math.random() * 100);
+        System.out.println("Random Number: " + randomNumber);
+        
+        // Addition
+        int a = 5;
+        int b = 3;
+        int sum = a + b;
+        System.out.println("Sum of a and b: " + sum);
+        
+        // Subtraction
+        int difference = a - b;
+        System.out.println("Difference of a and b: " + difference);
+        
+        // Multiplication
+        int product = a * b;
+        System.out.println("Product of a and b: " + product);
+    }
+}
